7cf5f88c5fca13f0078aab998c1ae185134e490f
Szznorlax <maycon344@yahoo.com>
2024-06-26 00:28:04 -0300

    Projeto do Felipão Atualizado!

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-let nameHero = "Red";
-let XP = 10999;
+const readline = require('readline');
 
-let level;
-if(XP < 1000){
-    level = "Ferro";
-}else if(XP < 2000){
-    level = "Bronze";
-}else if(XP < 5000){
-    level = "Prata";
-}else if(XP < 7000){
-    level = "Ouro";
-}else if(XP < 8000){
-    level = "Platina";
-}else if(XP < 9000){
-    level = "Ascendente";
-}else if(XP < 10000){
-    level = "Imortal";
-}else{
-    level = "Radiante";
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function Hero() {
+    rl.question("Digite o nome do herói: ", (nameHero) => {
+        rl.question("Digite a quantidade de experiência do herói: ", (XP) => {
+            const xpHero = parseInt(XP, 10);
+            let level;
+
+            if (xpHero < 1000) level = "Ferro";
+            else if (xpHero <= 2000) level = "Bronze";
+            else if (xpHero <= 5000) level = "Prata";
+            else if (xpHero <= 7000) level = "Ouro";
+            else if (xpHero <= 8000) level = "Platina";
+            else if (xpHero <= 9000) level = "Ascendente";
+            else if (xpHero <= 10000) level = "Imortal";
+            else level = "Radiante";
+
+            console.log(`O Herói de nome ${nameHero} está no nível de ${level}`);
+
+            rl.question("Você quer inserir outro herói? (sim/não): ", (newHero) => {
+                if (newHero.toLowerCase() === 'sim') {
+                    Hero(); 
+                } else {
+                    rl.close(); 
+                }
+            });
+        });
+    });
 }
 
-console.log(`O heroi ${nameHero} esta no nível ${level}!`);
+Hero(); 
